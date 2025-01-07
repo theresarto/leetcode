d56ffb3a33ae47b6696e98b72d36de732a6ebc6c
Theresa <138946854+theresarto@users.noreply.github.com>
2025-01-07 11:16:50 +0000

    Time: 130 ms (35.86%) | Memory: 18.2 MB (27.71%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if t == "":
+            return ""
+
+        window = defaultdict(int)  # {c:count}
+        dict_t = {c: t.count(c) for c in t}
+        have, need = 0, len(t)  # at start, don't have what we need yet
+        l = 0
+        resLen, res= float("inf"), [-1, -1]
+
+        for r in range(len(s)):
+            if s[r] in dict_t:
+                window[s[r]] += 1
+
+                if window[s[r]] <= dict_t[s[r]]:
+                    have += 1
+
+            while have == need:
+                if (r - l + 1) < resLen:
+                    res = [l, r]
+                    resLen = r-l+1
+                    # print(f"{res=} {resLen= }")
+
+                window[s[l]] -= 1
+                if s[l] in dict_t and window[s[l]] < dict_t[s[l]]:
+                    have -= 1
+                l += 1
+
+        l, r = res
+        return s[l : r + 1] if resLen != float("infinity") else ""
+
