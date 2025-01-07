c123cedfa63c7fe4feaf16870c404a06b241f236
Theresa <138946854+theresarto@users.noreply.github.com>
2025-01-07 11:22:10 +0000

    Time: 119 ms (41.04%) | Memory: 18.1 MB (27.71%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,20 +1,17 @@
-from collections import defaultdict
-
-
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         if t == "":
             return ""
 
-        window = defaultdict(int)  # {c:count}
+        window = {}  # {c:count}
         dict_t = {c: t.count(c) for c in t}
         have, need = 0, len(t)  # at start, don't have what we need yet
         l = 0
-        resLen, res= float("inf"), [-1, -1]
+        resLen, res = float("inf"), [-1, -1]
 
         for r in range(len(s)):
             if s[r] in dict_t:
-                window[s[r]] += 1
+                window[s[r]] = 1 + window.get(s[r], 0)
 
                 if window[s[r]] <= dict_t[s[r]]:
                     have += 1
@@ -22,14 +19,14 @@ class Solution:
             while have == need:
                 if (r - l + 1) < resLen:
                     res = [l, r]
-                    resLen = r-l+1
+                    resLen = r - l + 1
                     # print(f"{res=} {resLen= }")
 
-                window[s[l]] -= 1
-                if s[l] in dict_t and window[s[l]] < dict_t[s[l]]:
-                    have -= 1
+                if s[l] in dict_t:
+                    window[s[l]] -= 1
+                    if window[s[l]] < dict_t[s[l]]:
+                        have -= 1
                 l += 1
 
         l, r = res
-        return s[l : r + 1] if resLen != float("infinity") else ""
-
+        return s[l: r + 1] if resLen != float("infinity") else ""
